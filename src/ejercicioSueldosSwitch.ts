f650b8fe1bd0f55b9abc4b1290b2d69a13679940
EvYan1 <104235095+EvYan1@users.noreply.github.com>
2022-05-05 15:09:36 -0300

    cambie nombre variables

--- src/ejercicioSueldosSwitch.ts
+++ src/ejercicioSueldosSwitch.ts
@@ -1,27 +1,27 @@
-let sueldo12: number = Number(prompt("ingrese su sueldo mensual"));
+let sueldo: number = Number(prompt("ingrese su sueldo mensual"));
 //ingreso por teclado un numero sin puntos
-let total3: number;
+let total: number;
 //declaro la variable total para mostrar el resultado
 switch (true) {
-  case sueldo12 > 0 && sueldo12 <= 15000:
+  case sueldo > 0 && sueldo <= 15000:
     //en caso de que el sueldo sea entre 0 y 15000
-    total3 = sueldo12 * 1.2;
+    total = sueldo * 1.2;
     // la variable total cambia agregando el 20 porciendo al sueldo ingresado
-    console.log("su sueldo actualizado es de ", total3);
+    console.log("su sueldo actualizado es de ", total);
     //  muestra un mensaje al usuario con el sueldo actualizado
     break;
-  case sueldo12 >= 15001 && sueldo12 <= 20000:
+  case sueldo >= 15001 && sueldo <= 20000:
     //en caso de que el sueldo sea entre 15001 y 20000
-    total3 = sueldo12 * 1.1;
+    total = sueldo * 1.1;
     // la variable total cambia agregando el 10 porciendo al sueldo ingresado
-    console.log("su sueldo actualizado es de ", total3);
+    console.log("su sueldo actualizado es de ", total);
     //  muestra un mensaje al usuario con el sueldo actualizado
     break;
-  case sueldo12 <= 25000:
+  case sueldo <= 25000:
     //en caso de que el sueldo menor a 25000
-    total3 = sueldo12 * 0.05 + sueldo12;
+    total = sueldo * 0.05 + sueldo;
     // la variable total cambia agregando el 5 porciendo al sueldo ingresado
-    console.log("su sueldo actualizado es de ", total3);
+    console.log("su sueldo actualizado es de ", total);
     // muestra un mensaje al usuario con el sueldo actualizado
     break;
   default:
